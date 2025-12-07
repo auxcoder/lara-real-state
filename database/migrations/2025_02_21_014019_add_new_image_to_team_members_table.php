f1254cb4474208dae63d8617f63f3dc79765a1f0
Pavel Duran <auxcoder@gmail.com>
2025-12-07 01:21:16 -0500

    Clean migrations and improve seeds

--- database/migrations/2025_02_21_014019_add_new_image_to_team_members_table.php
+++ database/migrations/2025_02_21_014019_add_new_image_to_team_members_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::table('team_members', function (Blueprint $table) {
             //
-			$table->string('image')->nullable();
+            $table->string('image')->nullable();
         });
     }
 
@@ -24,7 +24,7 @@ return new class extends Migration
     {
         Schema::table('team_members', function (Blueprint $table) {
             //
-			 $table->dropColumn('image'); 
+            $table->dropColumn('image');
         });
     }
 };
